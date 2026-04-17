4559e2686ed8802baa64ba8d8170beeba37bc639
hadyaa552-debug <hadyaa552@gmail.com>
2026-04-17 17:59:26 +0200

    done

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-8 text-center border-t border-white/10 pb-48">
-      <a
-        href="https://www.topsuite.net/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-white transition-colors duration-300 text-xl text-primary"
-      >
-        تم التصميم والتطوير بواسطة Top Suite
-      </a>
+      <p className="text-sm text-gray-500">جميع الحقوق محفوظة © The Waterway Developments 2026</p>
     </footer>
   )
 }
